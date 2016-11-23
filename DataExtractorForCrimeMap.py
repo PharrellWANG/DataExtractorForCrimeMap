8b8fca6d5989d925e77d1ee18b76badc28686e27
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-24 01:49:47 +0800

    first commit

---
+++ DataExtractorForCrimeMap.py
@@ -0,0 +1,20 @@
+# data extractor for crime map #
+
+f = open('/Users/Pharrell_WANG/Documents/CrimeMapProject/Z-inputFilesForCrimeMap/listcrime.txt','r')
+
+for line in f:
+    crimelist = f.readlines()
+
+for i, singlecrime in enumerate(crimelist):
+    print( i, singlecrime )
+
+# inspiration for storing data pairs into database
+list1 = [1, 2, 3, 4, 5]
+list2 = [10, 20, 30, 40, 50]
+list3 = [100, 200, 300, 400, 500]
+for i, (l1, l2, l3) in enumerate(zip(list1, list2, list3)):
+    print(i, l1, l2, l3)
+
+f.close()
+
+
