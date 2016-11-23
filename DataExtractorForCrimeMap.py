0f2b0f8252857697fa025c8f96f0a6ae6129072d
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-24 03:21:42 +0800

    read lines without newline character, can do matching now

--- DataExtractorForCrimeMap.py
+++ DataExtractorForCrimeMap.py
@@ -1,18 +1,29 @@
 # data extractor for crime map #
 
-f = open('/Users/Pharrell_WANG/Documents/CrimeMapProject/Z-inputFilesForCrimeMap/listcrime.txt','r')
+with open('/Users/Pharrell_WANG/Documents/CrimeMapProject/Z-inputFilesForCrimeMap/listcrime.txt', 'r+') as fc:
+    crimelines = [line[:-1] for line in fc]
+print(type(crimelines))
 
-for line in f:
-    crimelist = f.readlines()
+with open('/Users/Pharrell_WANG/Documents/CrimeMapProject/Z-inputFilesForCrimeMap/listlocation.txt', 'r+') as fl:
+    localines = [line[:-1] for line in fl]
+print(type(localines))
 
-for i, singlecrime in enumerate(crimelist):
-    print( i, singlecrime )
-
+print(localines[8])
+print(localines[5])
+print(localines[8] + localines[5])
+print(crimelines[50])
+print(crimelines[23])
+print(crimelines[50] + crimelines[23])
+s = localines[9]
+print(s)
+t="西環"
+if s == t:
+    print ("equal")
+else:
+    print ("not equal")
 # inspiration for storing data pairs into database
-list1 = [1, 2, 3, 4, 5]
-list2 = [10, 20, 30, 40, 50]
-list3 = [100, 200, 300, 400, 500]
-for i, (l1, l2, l3) in enumerate(zip(list1, list2, list3)):
-    print(i, l1, l2, l3)
-
-f.close()
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [10, 20, 30, 40, 50]
+# list3 = [100, 200, 300, 400, 500]
+# for i, (l1, l2, l3) in enumerate(zip(list1, list2, list3)):
+#     print(i, l1, l2, l3)
