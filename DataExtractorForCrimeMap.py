f801e7e754d373199f2f3fb02946409628c24da7
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-25 01:24:25 +0800

    --->

--- DataExtractorForCrimeMap.py
+++ DataExtractorForCrimeMap.py
@@ -61,20 +61,22 @@ for member in collection:
     # refvalidlink = refpart1 + 's' + refpart2
     # refvalidlinkfinal = refvalidlink.encode('utf-8')
     # print(refvalidlinkfinal)
-    req = Request(
-        'http://www.hk01.com/01博評-政經社/56139/%E9%A6%99%E6%B8%AF%E6%88%AA%E7%8D%B2%E6%98%9F%E6%B4%B2%E8%A3%9D%E7%94%B2%E8%BB%8A-%E5%8F%B0%E6%96%B0%E8%BB%8D%E4%BA%8B%E5%90%88%E4%BD%9C%E5%8F%AF%E5%8F%97%E5%BD%B1%E9%9F%BF-'
 
-        ,
-        headers={'User-Agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    soup = BeautifulSoup(page, 'lxml')
-    # //--->for redirecting the web contents to text file  for the purpose of debugging??
-    orig_stdout = sys.stdout
-    f = open('output_of_inspect_single_news.html', 'w')
-    sys.stdout = f
-    print(soup.prettify())
-    sys.stdout = orig_stdout
-    f.close()
+    # req = Request(
+    #     'http://www.hk01.com/01博評-政經社/56139/%E9%A6%99%E6%B8%AF%E6%88%AA%E7%8D%B2%E6%98%9F%E6%B4%B2%E8%A3%9D%E7%94%B2%E8%BB%8A-%E5%8F%B0%E6%96%B0%E8%BB%8D%E4%BA%8B%E5%90%88%E4%BD%9C%E5%8F%AF%E5%8F%97%E5%BD%B1%E9%9F%BF-'
+    #
+    #     ,
+    #     headers={'User-Agent': 'Mozilla/5.0'})
+    # page = urlopen(req).read()
+    # soup = BeautifulSoup(page, 'lxml')
+    # # //--->for redirecting the web contents to text file  for the purpose of debugging??
+    # orig_stdout = sys.stdout
+    # f = open('output_of_inspect_single_news.html', 'w')
+    # sys.stdout = f
+    # print(soup.prettify())
+    # sys.stdout = orig_stdout
+    # f.close()
+
     # //--->redirection completed
 
     # for crime in crimelines:
