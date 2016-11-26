f4f48513d299cfbb4a2e466a89c1e8e38a7dc359
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-26 21:53:33 +0800

    connector optimized using config style

--- Connector.py
+++ Connector.py
@@ -2,8 +2,17 @@ import mysql.connector
 
 from mysql.connector import errorcode
 
+config = {
+    'user': 'root',
+    'password': '123456',
+    'host': '127.0.0.1',
+    'database': 'crimemap',
+    'raise_on_warnings': True,
+}
+
 try:
-    cnx = mysql.connector.connect(user='root', password='123456', host='127.0.0.1', database='crimemap')
+    # cnx = mysql.connector.connect(user='root', password='123456', host='127.0.0.1', database='crimemap')
+    cnx = mysql.connector.connect(**config)
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Hint: Something is wrong with your user name or password")
